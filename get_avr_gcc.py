7aef491982486c50217c4f9b970df24a6b8535b2
Fahri Ali Rahman <fahri@nodeflux.io>
2022-01-13 20:53:18 +0700

    update tarball builder scripts

--- get_avr_gcc.py
+++ get_avr_gcc.py
@@ -1,13 +1,15 @@
 import urllib.request
 import tarfile
+from distutils.dir_util import copy_tree
 from pathlib import Path
 
 url = "https://github.com/modm-io/avr-gcc/releases/download/v9.2.0/avr-gcc.tar.bz2"
-filename = "modm-avr-gcc.tar.bz2"
+filename = "avr-gcc.tar.bz2"
 urllib.request.urlretrieve(url,filename)
-tar = tarfile.open("modm-avr-gcc.tar.bz2", "r:bz2")  
+tar = tarfile.open(filename, "r:bz2")  
 tar.extractall("./")
 tar.close()
+Path(filename).unlink()
 
 # on avr gcc 9.2.0, the structure is:
 # avr-gcc
@@ -26,7 +28,12 @@ path = Path("avr-gcc/avr-gcc").absolute()
 parent_dir = path.parents[1]
 for inner_path in path.iterdir():
     inner_path.rename(parent_dir/inner_path.name)
-path.rmdir()
-path = Path("avr-gcc/avr-binutils")
-path.rename("avr-binutils")
-# Path(filename).unlink()
+copy_tree("avr-gcc/avr-binutils/avr/", "avr/")
+copy_tree("avr-gcc/avr-binutils/bin/", "avr/bin/")
+copy_tree("avr-gcc/avr-binutils/share/", "share/")
+
+import shutil
+shutil.rmtree('avr-gcc')
+
+import subprocess
+subprocess.call(["tar", "-cvjSf", "toolchain-avr-gcc.tar.bz2", "."])
